0871eb22a5d6ea40e7ab6366ae57d92bf27357de
MatthewFrench <matthewf@ksu.edu>
2015-06-24 02:28:41 -0500

    Added windows mastery key presser

---
+++ Mastery Key Presser/League Auto-Key Presser/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace League_Auto_Key_Presser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.autoKeyOn = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // autoKeyOn
+            // 
+            this.autoKeyOn.AutoSize = true;
+            this.autoKeyOn.Checked = true;
+            this.autoKeyOn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.autoKeyOn.Location = new System.Drawing.Point(12, 12);
+            this.autoKeyOn.Name = "autoKeyOn";
+            this.autoKeyOn.Size = new System.Drawing.Size(192, 17);
+            this.autoKeyOn.TabIndex = 6;
+            this.autoKeyOn.Text = "Mastery Key Presser On (control+6)";
+            this.autoKeyOn.UseVisualStyleBackColor = true;
+            this.autoKeyOn.CheckedChanged += new System.EventHandler(this.autoKeyOn_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 44);
+            this.Controls.Add(this.autoKeyOn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mastery Key Presser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox autoKeyOn;
+    }
+}
+
---
+++ Mastery Key Presser/League Auto-Key Presser/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Gma.UserActivityMonitor;
+using AutoItX3Lib;
+
+using System.Reflection;
+using System.Web;
+using System.Runtime;
+using System.Configuration;
+
+using System.Threading;
+
+namespace League_Auto_Key_Presser
+{
+    public partial class Form1 : Form
+    {
+        static AutoItX3 _autoIT = new AutoItX3();
+
+        ATimer timer = null;
+
+        Stopwatch lastPressedMastery = Stopwatch.StartNew();
+
+        int pressSpell1Interval = 9*1000;
+        
+        bool autoKeyOnBool = true;
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        static extern int NtSetTimerResolution(int DesiredResolution, bool SetResolution, out int CurrentResolution);
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // The requested resolution in 100 ns units:
+            int DesiredResolution = 1000;
+            // Note: The supported resolutions can be obtained by a call to NtQueryTimerResolution()
+
+            int CurrentResolution = 0;
+
+            // 1. Requesting a higher resolution
+            // Note: This call is similar to timeBeginPeriod.
+            // However, it to to specify the resolution in 100 ns units.
+            if (NtSetTimerResolution(DesiredResolution, true, out CurrentResolution) != 0)
+            {
+                Console.WriteLine("Setting resolution failed");
+            }
+            else
+            {
+                Console.WriteLine("CurrentResolution [100 ns units]: " + CurrentResolution);
+            }
+
+            ATimer.ElapsedTimerDelegate callback = timer_Tick;
+            timer = new ATimer(3, 1, callback);
+            timer.Start();
+            
+
+            _autoIT.AutoItSetOption("SendKeyDelay", 0);
+
+            _autoIT.AutoItSetOption("SendKeyDownDelay", 0);
+        }
+        void timer_Tick()
+        {
+            if (autoKeyOnBool)
+            {
+                
+                    if (lastPressedMastery.ElapsedMilliseconds >=1000)
+                    {
+                    lastPressedMastery.Restart();
+                    tapMastery();
+                    }
+            }
+        }
+        
+        void tapMastery()
+        {
+                _autoIT.Send("^6"); 
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void autoKeyOn_CheckedChanged(Object sender, EventArgs e)
+        {
+            autoKeyOnBool = ((CheckBox)sender).Checked;
+        }
+    }
+    
+}
